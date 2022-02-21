211b1264f1aeb6b3f9c95092acf662be58198ec2
exthitikorn <rockerzeeza@gmail.com>
2022-02-21 14:20:05 +0700

    create customer controller

---
+++ backend/api/controllers/customerController.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const cus = mongoose.model('customer');
+
+exports.list_all_cus = (req, res)=>{
+    cus.find({},(err,cuss)=>{
+        if(err) res.send(err);
+        res.json(cuss);
+    }).populate('sdp')
+}
+
+exports.create_a_cus = (req, res)=>{
+    const newCus = new cus(req.body);
+    newCus.save((err, cus)=>{
+        if(err) res.send(err);
+        res.json(cus);
+    })
+}
+
+exports.read_a_cus = (req, res)=>{
+    cus.findById(req.params.cusId,(err, cus)=>{
+        if(err) res.send(err);
+        res.json(cus);
+    }).populate('sdp')
+}
+
+exports.update_a_cus = (req, res)=> {
+    cus.findByIdAndUpdate(
+        {_id: req.params.cusId},
+        req.body,
+        {new: true},
+        (err, cus)=>{
+            if(err) res.send(err);
+            res.json(cus);
+        }
+    )
+}
+
+exports.delete_a_cus = (req, res)=>{
+    cus.deleteOne({_id: req.params.cusId}, err =>{
+        if(err) res.send(err);
+        res.json({
+            message:"Customer Succesfully Deleted",
+            _id: req.params.cusId
+        })
+    })
+}
