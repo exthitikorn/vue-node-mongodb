051d48010c2dd2ff047df3efa434036d70af6f0d
exthitikorn <rockerzeeza@gmail.com>
2022-03-02 10:41:44 +0700

    calclate dinstance function

--- src/views/DistanceSdp.vue
+++ src/views/DistanceSdp.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
     <h1>SDP around me</h1>
-    <div v-if="dataDist.length > 0">
+    <div v-if="dataLoc.length > 0">
       <table class="ui celled compact table">
         <thead>
           <tr>
             <th><i class=""></i>SDP name</th>
-            <!-- <th><i class=""></i>Distance</th> -->
+            <th><i class=""></i>Distance</th>
             <th class="center aligned">
               <i class="map marker alternate icon"></i>
             </th>
           </tr>
         </thead>
-        <tr v-for="(dataDist, i) in dataDist" :key="i">
-          <td>{{ dataDist.sdp_Name }}</td>
-          <!-- <td>{{ dataDist.dist }} Meter</td> -->
+        <tr v-for="(dataLoc, i) in even(dataLoc)" :key="i">
+          <td>{{ dataLoc.name }}</td>
+          <td>{{ dataLoc.dist }} Meter</td>
           <td width="120" class="center aligned">
             <router-link
-              :to="{ name: 'google-map-sdp', params: { id: dataDist._id } }"
+              :to="{ name: 'google-map-sdp', params: { id: dataLoc._id } }"
               >Location</router-link
             >
           </td>
@@ -40,67 +40,82 @@ export default {
       sdps: [],
       currentLocation: { lat: null, lng: null },
       dataDist: [],
-      // loc: [],
+      dataLoc: [],
     };
   },
   methods: {
-    //detects location from browser & calculate distance
+    //Sort data
+    sortedDataLoc() {
+      return this.dataLoc.sort((a, b) => a.dist - b.dist);
+    },
+    //test
+    even: function(arr) {
+      // Set slice() to avoid to generate an infinite loop!
+      return arr.slice().sort(function(a, b) {
+        return a.dist - b.dist;
+      });
+    },
+    //detects location from browser
     geolocate() {
       navigator.geolocation.getCurrentPosition((position) => {
         this.currentLocation = {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         };
-        // this.loc.push(this.currentLocation.lng);
-        // this.loc.push(this.currentLocation.lat);
-
-        // for (let i = 0; i < this.sdps.length; i++) {
-        //   var lat1 = position.coords.latitude;
-        //   var lng1 = position.coords.longitude;
-        //   var lat2 = this.sdps[i].loc[1];
-        //   var lng2 = this.sdps[i].loc[0];
-        //   if (lat1 == lat2 && lng1 == lng2) {
-        //     this.dataDist.push({
-        //       _id: this.sdps[i]._id,
-        //       name: this.sdps[i].sdp_Name,
-        //       lat: lat1,
-        //       lng: lng1,
-        //       dist: 0,
-        //     });
-        //   } else {
-        //     var radlat1 = (Math.PI * lat1) / 180;
-        //     var radlat2 = (Math.PI * lat2) / 180;
-        //     var theta = lng1 - lng2;
-        //     var radtheta = (Math.PI * theta) / 180;
-        //     var dist =
-        //       Math.sin(radlat1) * Math.sin(radlat2) +
-        //       Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-        //     if (dist > 1) {
-        //       dist = 1;
-        //     } else {
-        //       dist = Math.acos(dist);
-        //       dist = (dist * 180) / Math.PI;
-        //       dist = dist * 60 * 1.1515;
-        //       dist = dist * 1609.344;
-        //       if (dist < 500) {
-        //         this.dataDist.push({
-        //           _id: this.sdps[i]._id,
-        //           name: this.sdps[i].sdp_Name,
-        //           lat: lat1,
-        //           lng: lng1,
-        //           dist: dist.toFixed(0),
-        //         });
-        //       }
-        //     }
-        //   }
-        // }
       });
+    },
+    //Calculate distance
+    distance() {
+      for (let i = 0; i < this.dataDist.length; i++) {
+        var lat1 = this.currentLocation.lat;
+        var lng1 = this.currentLocation.lng;
+        var lat2 = this.dataDist[i].loc[1];
+        var lng2 = this.dataDist[i].loc[0];
+        if (lat1 == lat2 && lng1 == lng2) {
+          this.dataLoc.push({
+            _id: this.dataDist[i]._id,
+            name: this.dataDist[i].sdp_Name,
+            lat: lat1,
+            lng: lng1,
+            dist: 0,
+          });
+        } else {
+          var radlat1 = (Math.PI * lat1) / 180;
+          var radlat2 = (Math.PI * lat2) / 180;
+          var theta = lng1 - lng2;
+          var radtheta = (Math.PI * theta) / 180;
+          var dist =
+            Math.sin(radlat1) * Math.sin(radlat2) +
+            Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+          if (dist > 1) {
+            dist = 1;
+          } else {
+            dist = Math.acos(dist);
+            dist = (dist * 180) / Math.PI;
+            dist = dist * 60 * 1.1515;
+            dist = dist * 1609.344;
+            if (dist < 500) {
+              this.dataLoc.push({
+                _id: this.dataDist[i]._id,
+                name: this.dataDist[i].sdp_Name,
+                lat: lat1,
+                lng: lng1,
+                dist: dist.toFixed(0),
+              });
+            }
+          }
+        }
+      }
     },
   },
   async mounted() {
     // this.sdps = await api.getsdps();
     this.geolocate();
-    this.dataDist = await api.distance(this.$route.params.lng, this.$route.params.lat);
+    this.dataDist = await api.distance(
+      this.$route.params.lng,
+      this.$route.params.lat
+    );
+    this.distance();
   },
 };
 </script>
