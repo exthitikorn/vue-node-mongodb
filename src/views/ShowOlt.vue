1ae99c87ee3ac3956b79529ee3d2d5ecfd9d1344
exthitikorn <rockerzeeza@gmail.com>
2022-03-23 16:06:30 +0700

    edit table in show pon and ofccc page

--- src/views/ShowOlt.vue
+++ src/views/ShowOlt.vue
@@ -2,30 +2,33 @@
   <div>
     <h1>PON : {{ pons.length }}</h1>
     <flash-message></flash-message>
+    <br />
     <div v-if="pons.length > 0">
-      <table id="pons" class="ui celled table">
+      <div class="tableresponsive" style="overflow-x: auto">
+        <table id="pons" class="ui padded table">
         <thead>
           <tr>
-            <th><i class=""></i>PON Name</th>
-            <th><i class=""></i>Rack</th>
-            <th><i class=""></i>Shelf</th>
-            <th><i class=""></i>Slot</th>
-            <th><i class=""></i>Port</th>
+            <th class="center aligned"><i class=""></i>PON Name</th>
+            <th class="center aligned"><i class=""></i>Rack</th>
+            <th class="center aligned"><i class=""></i>Shelf</th>
+            <th class="center aligned"><i class=""></i>Slot</th>
+            <th class="center aligned"><i class=""></i>Port</th>
           </tr>
         </thead>
         <tr v-for="(pon, i) in pons" :key="i">
-          <td>{{ pon.pon_Name }}</td>
-          <td>{{ pon.pon_Rack }}</td>
-          <td>{{ pon.pon_Shelf }}</td>
-          <td>{{ pon.pon_Slot }}</td>
-          <td>{{ pon.pon_Port }}</td>
+          <td class="center aligned" data-title="ชื่อ">{{ pon.pon_Name }}</td>
+          <td class="center aligned" data-title="rack">{{ pon.pon_Rack }}</td>
+          <td class="center aligned" data-title="shelf">{{ pon.pon_Shelf }}</td>
+          <td class="center aligned" data-title="slot">{{ pon.pon_Slot }}</td>
+          <td class="center aligned" data-title="port">{{ pon.pon_Port }}</td>
         </tr>
       </table>
-      <div class="container6">
-        <a onclick="history.back()" class="negative ui button">Back</a>
       </div>
     </div>
     <div v-else>You don't have any PON!.</div>
+    <div class="container6">
+        <a onclick="history.back()" class="negative ui button">Back</a>
+      </div>
   </div>
 </template>
 
@@ -45,6 +48,59 @@ export default {
 </script>
 
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.tableresponsive {
+  margin: 15px auto;
+}
+
+@media only screen and (max-width: 770px) {
+  .tableresponsive table,
+  .tableresponsive thead,
+  .tableresponsive tbody,
+  .tableresponsive th,
+  .tableresponsive td,
+  .tableresponsive tr {
+    display: block;
+  }
+
+  .tableresponsive thead tr {
+    position: absolute;
+    top: -9999px;
+    left: -9999px;
+  }
+
+  .tableresponsive tr {
+    border: 1px solid #ccc;
+  }
+
+  .tableresponsive td {
+    border: none;
+    border-bottom: 1px solid #eee;
+    position: relative;
+    padding-left: 45% !important;
+    white-space: normal;
+    text-align: left;
+  }
+
+  .tableresponsive td:before {
+    position: absolute;
+    top: 6px;
+    left: 6px;
+    width: 50%;
+    padding-right: 10px;
+    white-space: nowrap;
+    text-align: left;
+    font-weight: bold;
+  }
+
+  .tableresponsive td:before {
+    content: attr(data-title);
+  }
+}
 .actions a {
   display: block;
   text-decoration: underline;
--- src/views/ShowPon.vue
+++ src/views/ShowPon.vue
@@ -3,32 +3,39 @@
     <h1>ofccc : {{ ofcccs.length }}</h1>
     <flash-message></flash-message>
     <div v-if="ofcccs.length > 0">
-      <table id="ofcccs" class="ui celled table">
-        <thead>
-          <tr>
-            <th>ofccc Name</th>
-            <th>Type</th>
-            <th>Latitude</th>
-            <th>Longitude</th>
-            <th class="center aligned"><i class="map marker alternate icon"></i>Location</th>
+      <div class="tableresponsive" style="overflow-x: auto">
+        <table id="ofcccs" class="ui padded table">
+          <thead>
+            <tr>
+              <th class="center aligned">ofccc Name</th>
+              <th class="center aligned">Type</th>
+              <th class="center aligned">Latitude</th>
+              <th class="center aligned">Longitude</th>
+              <th class="center aligned">
+                <i class="map marker alternate icon"></i>Location
+              </th>
+            </tr>
+          </thead>
+          <tr v-for="(ofccc, i) in ofcccs" :key="i">
+            <td class="center aligned" data-title="ชื่อ">{{ ofccc.ofccc_Name }}</td>
+            <td class="center aligned" data-title="ประเภท">{{ ofccc.ofccc_Type }}</td>
+            <td class="center aligned" data-title="ละติจูด">{{ ofccc.ofccc_Lat }}</td>
+            <td class="center aligned" data-title="ลองจิจูด">{{ ofccc.ofccc_Lng }}</td>
+            <td class="center aligned" data-title="ตำแหน่ง">
+              <router-link
+                class="ui teal button width-120px"
+                :to="{ name: 'google-map-ofccc', params: { id: ofccc._id } }"
+                >Location</router-link
+              >
+            </td>
           </tr>
-        </thead>
-        <tr v-for="(ofccc, i) in ofcccs" :key="i">
-          <td>{{ ofccc.ofccc_Name }}</td>
-          <td>{{ ofccc.ofccc_Type }}</td>
-          <td>{{ ofccc.ofccc_Lat }}</td>
-          <td>{{ ofccc.ofccc_Lng }}</td>
-          <td class="center aligned">
-            <router-link class="ui teal button" :to="{ name: 'google-map-ofccc', params: { id: ofccc._id } }"
-              >Location</router-link>
-          </td>
-        </tr>
-      </table>
-      <div class="container6">
-        <a onclick="history.back()" class="negative ui button">Back</a>
+        </table>
       </div>
     </div>
     <div v-else>You don't have any ofccc!.</div>
+    <div class="container6">
+      <a onclick="history.back()" class="negative ui button">Back</a>
+    </div>
   </div>
 </template>
 
@@ -48,6 +55,62 @@ export default {
 </script>
 
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.tableresponsive {
+  margin: 15px auto;
+}
+
+@media only screen and (max-width: 770px) {
+  .tableresponsive table,
+  .tableresponsive thead,
+  .tableresponsive tbody,
+  .tableresponsive th,
+  .tableresponsive td,
+  .tableresponsive tr {
+    display: block;
+  }
+
+  .tableresponsive thead tr {
+    position: absolute;
+    top: -9999px;
+    left: -9999px;
+  }
+
+  .tableresponsive tr {
+    border: 1px solid #ccc;
+  }
+
+  .tableresponsive td {
+    border: none;
+    border-bottom: 1px solid #eee;
+    position: relative;
+    padding-left: 45% !important;
+    white-space: normal;
+    text-align: left;
+  }
+
+  .tableresponsive td:before {
+    position: absolute;
+    top: 6px;
+    left: 6px;
+    width: 50%;
+    padding-right: 10px;
+    white-space: nowrap;
+    text-align: left;
+    font-weight: bold;
+  }
+
+  .tableresponsive td:before {
+    content: attr(data-title);
+  }
+}
+.width-120px {
+  width: 120px;
+}
 .actions a {
   display: block;
   text-decoration: underline;
