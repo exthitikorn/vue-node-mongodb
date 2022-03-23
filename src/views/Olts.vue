f42bb41620459789f5d6e5acc3048c03438f07b7
exthitikorn <rockerzeeza@gmail.com>
2022-03-23 11:34:59 +0700

    edit OLT table to responsive table

--- src/views/Olts.vue
+++ src/views/Olts.vue
@@ -3,40 +3,50 @@
     <h1>OLT : {{ olts.length }}</h1>
     <a href="/olts/new" class="ui blue button">New</a>
     <flash-message></flash-message>
-    <br>
+    <br />
     <div v-if="olts.length > 0">
-      <!-- <div class="ui input fluid">
-        <input type="text" v-model="search" placeholder="Search">
-      </div> -->
-      <table id="olts" class="ui celled table">
-        <thead>
-          <tr>
-            <th>OLT Name</th>
-            <th class="center aligned"><i class="map marker alternate icon"></i></th>
-            <th class="center aligned"><i class="info circle icon"></i></th>
-            <th class="center aligned"><i class="edit icon"></i></th>
-            <th class="center aligned"><i class="trash icon"></i></th>
+      <div style="overflow-x: auto">
+        <table id="olts" class="ui padded table" role="table">
+          <thead role="rowgroup">
+            <tr role="row">
+              <th class="center aligned" role="columnheader">OLT Name</th>
+              <th class="center aligned" role="columnheader"><i class="map marker alternate icon"></i></th>
+              <th class="center aligned" role="columnheader"><i class="info circle icon"></i></th>
+              <th class="center aligned" role="columnheader"><i class="edit icon"></i></th>
+              <th class="center aligned" role="columnheader"><i class="trash icon"></i></th>
+            </tr>
+          </thead>
+          <tr v-for="(olt, i) in olts" :key="i" role="row">
+            <td width="600" role="cell">{{ olt.olt_Name }}</td>
+            <td class="center aligned" role="cell">
+              <router-link
+                class="ui teal button"
+                :to="{ name: 'google-map-olt', params: { id: olt._id } }"
+                >Location</router-link
+              >
+            </td>
+            <td class="center aligned" role="cell">
+              <router-link
+                class="ui green button"
+                :to="{ name: 'show-pon-olt', params: { id: olt._id } }"
+                >Show PON</router-link
+              >
+            </td>
+            <td class="center aligned" role="cell">
+              <router-link
+                class="ui orange button"
+                :to="{ name: 'edit-olt', params: { id: olt._id } }"
+                >Edit</router-link
+              >
+            </td>
+            <td class="center aligned" @click.prevent="onDestroy(olt._id)" role="cell">
+              <a class="ui red button" :href="`/olts/${olt._id}`"
+                >Delete</a
+              >
+            </td>
           </tr>
-        </thead>
-        <tr v-for="(olt, i) in olts" :key="i">
-          <td>{{ olt.olt_Name }}</td>
-          <td width="75" class="center aligned">
-            <router-link class="ui teal button" :to="{ name: 'google-map-olt', params: { id: olt._id } }"
-              >Location</router-link>
-          </td>
-          <td width="150" class="center aligned">
-            <router-link class="ui green button" :to="{ name: 'show-pon-olt', params: { id: olt._id } }"
-              >Show PON</router-link>
-          </td>
-          <td width="75" class="center aligned">
-            <router-link class="ui orange button" :to="{ name: 'edit-olt', params: { id: olt._id } }"
-              >Edit</router-link>
-          </td>
-          <td width="75" class="center aligned" @click.prevent="onDestroy(olt._id)">
-            <a class="ui red button" :href="`/olts/${olt._id}`">Delete</a>
-          </td>
-        </tr>
-      </table>
+        </table>
+      </div>
     </div>
     <div v-else>You don't have any OLT!.</div>
   </div>
@@ -69,5 +79,71 @@ export default {
 </script>
 
 <style scoped>
+/*
+	Max width before this PARTICULAR table gets nasty. This query will take effect for any screen smaller than 760px and also iPads specifically.
+	*/
+@media only screen and (max-width: 760px),
+  (min-device-width: 768px) and (max-device-width: 1024px) {
+  /* Force table to not be like tables anymore */
+  table,
+  thead,
+  tbody,
+  th,
+  td,
+  tr {
+    display: block;
+  }
 
+  /* Hide table headers (but not display: none;, for accessibility) */
+  thead tr {
+    position: absolute;
+    top: -9999px;
+    left: -9999px;
+  }
+
+  tr {
+    margin: 0 0 1rem 0;
+  }
+
+  tr:nth-child(odd) {
+    background: #ccc;
+  }
+
+  td {
+    /* Behave  like a "row" */
+    border: none;
+    border-bottom: 1px solid #eee;
+    position: relative;
+    padding-left: 50%;
+  }
+
+  td:before {
+    /* Now like a table header */
+    position: absolute;
+    /* Top/left values mimic padding */
+    top: 0;
+    left: 6px;
+    width: 45%;
+    padding-right: 10px;
+    white-space: nowrap;
+  }
+
+  /*
+		Label the data
+    You could also use a data-* attribute and content for this. That way "bloats" the HTML, this way means you need to keep HTML and CSS in sync. Lea Verou has a clever way to handle with text-shadow.
+		*/
+  
+  td:nth-of-type(2):before {
+    content: "Location";
+  }
+  td:nth-of-type(3):before {
+    content: "Info";
+  }
+  td:nth-of-type(4):before {
+    content: "Edit";
+  }
+  td:nth-of-type(5):before {
+    content: "Delete";
+  }
+}
 </style>
